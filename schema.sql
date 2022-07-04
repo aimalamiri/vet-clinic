bdcb0ee905c527b5e8f3df9fefb9934c69fc0781
Aimal Amiri <meaimal@hotmail.com>
2022-07-04 18:04:36 +0430

    Add queries for inserting data into animals table

--- schema.sql
+++ schema.sql
@@ -5,5 +5,6 @@ CREATE TABLE animals (
     date_of_birth date NOT NULL,
     escape_attempts INT,
     neutered BOOLEAN NOT NULL,
-    weight_kg NUMERIC(4, 2) NOT NULL
+    weight_kg NUMERIC(4, 2) NOT NULL,
+    PRIMARY KEY(id)
 );
