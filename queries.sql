25ddfae8fe3dc5a58c20bd94a9a2d43c94a4fdc8
Aimal Amiri <meaimal@hotmail.com>
2022-07-07 20:35:40 +0430

    Add join queries for many to many relationships

--- queries.sql
+++ queries.sql
@@ -58,4 +58,13 @@ SELECT * FROM animals a JOIN owners o ON a.owner_id=o.id JOIN species s ON a.spe
 SELECT * FROM animals a JOIN owners o ON o.id=a.owner_id WHERE a.escape_attempts=0;
 SELECT o.full_name, COUNT(a.id) FROM owners o LEFT OUTER JOIN animals a ON o.id=a.owner_id GROUP BY o.full_name ORDER BY count DESC LIMIT 1;
 
+SELECT a.name, v.date_of_visit from animals a JOIN visits v ON a.id=v.animal_id JOIN vets ve ON ve.id=v.vet_id WHERE ve.name='William Tatcher' ORDER BY v.date_of_visit DESC LIMIT 1;
+SELECT COUNT(a.name) FROM animals a JOIN visits v ON a.id=v.animal_id JOIN vets ve ON ve.id=v.vet_id WHEREve.name='Stephanie Mendez';
+SELECT v.name, v.age, v.date_of_graduation, sp.name FROM vets v LEFT JOIN specializations s ON v.id=s.vet_id LEFT JOIN species sp ON sp.id=s.specie_id;
+SELECT a.* FROM animals a JOIN visits v ON a.id=v.animal_id JOIN vets ve ON ve.id=v.vet_id WHERE date_of_visit <= '2020-08-30' AND date_of_visit >= '2020-04-01';
+SELECT a.name, COUNT(v.animal_id) FROM animals a JOIN visits v ON a.id=v.animal_id GROUP BY a.name ORDER BY count DESC LIMIT 1;
+SELECT a.name, MIN(v.date_of_visit) FROM animals a JOIN visits v ON v.animal_id=a.id JOIN vets ve ON v.vet_id=ve.id WHERE ve.name='Maisy Smith' GROUP BY a.name ORDER BY min LIMIT 1;
+SELECT a.*, ve.*, v.date_of_visit FROM animals a JOIN visits v ON v.animal_id=a.id JOIN vets ve ON ve.id=v.vet_id ORDER BY date_of_visit LIMIT 1;
+SELECT COUNT(*) FROM vets v LEFT JOIN specializations s ON s.vet_id=v.id JOIN visits vi ON vi.vet_id=v.id WHERE s.vet_id IS null;
+SELECT s.name FROM species s JOIN (SELECT COUNT(a), a.species_id FROM vets v JOIN visits vi ON vi.vet_id=v.id JOIN animals a ON a.id=vi.animal_id WHERE v.name='Maisy Smith' GROUP BY a.species_id ORDER BY count DESC LIMIT 1) ms ON s.id=ms.species_id;
 
