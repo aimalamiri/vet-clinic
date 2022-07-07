518691166e835d94481d7b356f2c6f49cda60ef7
Aimal Amiri <meaimal@hotmail.com>
2022-07-07 16:49:26 +0430

    Add vets table with specilizations and visits link tables

--- schema.sql
+++ schema.sql
@@ -23,10 +23,40 @@ CREATE TABLE species(
 );
 
 ALTER TABLE animals DROP COLUMN id;
-ALTER TABLE animals ADD COLUMN id INT GENERATED ALWAYS AS IDENTITY;
-ALTER TABLE animals ADD PRIMARY KEY(id);
+ALTER TABLE animals
+ADD COLUMN id INT GENERATED ALWAYS AS IDENTITY;
+ALTER TABLE animals
+ADD PRIMARY KEY(id);
 ALTER TABLE animals DROP COLUMN species;
-ALTER TABLE animals ADD COLUMN species_id INT;
-ALTER TABLE animals ADD CONSTRAINT species_fk FOREIGN KEY(species_id) REFERENCES species(id);
-ALTER TABLE animals ADD COLUMN owner_id INT;
-ALTER TABLE animals ADD CONSTRAINT owners_fk FOREIGN KEY(owner_id) REFERENCES owners(id);
+ALTER TABLE animals
+ADD COLUMN species_id INT;
+ALTER TABLE animals
+ADD CONSTRAINT species_fk FOREIGN KEY(species_id) REFERENCES species(id);
+ALTER TABLE animals
+ADD COLUMN owner_id INT;
+ALTER TABLE animals
+ADD CONSTRAINT owners_fk FOREIGN KEY(owner_id) REFERENCES owners(id);
+
+CREATE TABLE vets (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(100) NOT NULL,
+    age INT,
+    date_of_graduation DATE,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE specializations (
+    vet_id INT NOT NULL,
+    specie_id INT NOT NULL,
+    CONSTRAINT vets_fk FOREIGN KEY(vet_id) REFERENCES vets(id),
+    CONSTRAINT species_fk FOREIGN KEY(specie_id) REFERENCES species(id)
+);
+
+CREATE TABLE visits (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    vet_id INT NOT NULL,
+    animal_id INT NOT NULL,
+    date_of_visit DATE,
+    CONSTRAINT vets_fk FOREIGN KEY(vet_id) REFERENCES vets(id),
+    CONSTRAINT animals_fk FOREIGN KEY(animal_id) REFERENCES animals(id)
+);
